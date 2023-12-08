2accf3a1ccf9664cd24bce69b12abbb44db1b29a
Dan King <king.daniel.thomas@gmail.com>
2023-12-08 15:13:09 +0700

    My node routing

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('node:http')
+const url = require('node:url')
+const fs = require('node:fs')
+
+http.createServer((req, res) => {
+  let reqUrl = url.parse(req.url).pathname
+  if (reqUrl.includes('favicon')) { // temp hack
+    return;
+  } else if (reqUrl === '/' || reqUrl === '') {
+    reqUrl = '/index';
+    getHtmlFile(reqUrl)
+  } else {
+    getHtmlFile(reqUrl)
+  }
+
+  function getHtmlFile(reqUrl){
+    fs.readFile(`./${reqUrl}.html`, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        notFound = fs.readFile('./404.html', (err, notFound) => {
+          console.error(err);
+          res.end(notFound);
+        })
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    })
+  }
+}).listen(8080)
+
